43d74e262cffd960d65e9f2ecf9288565ab80e28
yoon-dh <acttoze1@gmail.com>
2023-07-08 18:36:01 +0900

    vanilla-redux todo-app : add todo

---
+++ vanilla-redux-todo/.history/src/index_20230708182718.js
@@ -0,0 +1,5 @@
+import { legacy_createStore as createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
---
+++ vanilla-redux-todo/.history/src/index_20230708183013.js
@@ -0,0 +1,21 @@
+import { legacy_createStore as createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {};
+
+const store = createStore(reducer);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
+};
+
+form.addEventListener("submit", onSubmit);
